9af48fc34047324cddd90db0ab3584e060316573
Ritual <github@unbo.lt>
2014-10-17 14:10:13 +0100

    Adding Bootstrap JS into functions rather than directly into header

--- functions.php
+++ functions.php
@@ -20,3 +20,14 @@ if (function_exists('add_theme_support'))
     // Enables post and comment RSS feed links to head
     add_theme_support('automatic-feed-links');
 }
+
+function seekthehorizon_header_scripts() {
+    if ($GLOBALS['pagenow'] != 'wp-login.php' && !is_admin()) {
+
+        wp_register_script('bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js', false, '3.2.0'); // Bootstrap
+        wp_enqueue_script('bootstrap');
+
+        wp_register_script('seekthehorizonscripts', get_template_directory_uri() . '/js/seekthehorizon.js', array('jquery'), '1.0.0'); // Custom scripts
+        wp_enqueue_script('seekthehorizonscripts');
+    }
+}
