462831ef40d9510b2c78d7c9382371ca99c41899
Dan Baker <dan@unbo.lt>
2014-10-17 11:53:38 +0100

    Adding initial header

---
+++ header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+	<head>
+		<meta charset="<? bloginfo('charset'); ?>">
+		
+		<link href="//www.google-analytics.com" rel="dns-prefetch">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	</head>
+	<body <? body_class(); ?>>
